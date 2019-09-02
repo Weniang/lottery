58f856cdc70f4329c811f4811acbddc9fcf2194f
zohar <1025719411@qq.com>
2019-09-02 18:42:28 +0800

    拆分业务模块

--- lottery-admin/src/main/java/me/zohar/lottery/config/security/WebSecurityConfig.java
+++ lottery-admin/src/main/java/me/zohar/lottery/config/security/WebSecurityConfig.java
@@ -1,49 +1,48 @@
 package me.zohar.lottery.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private CustomAuthenticationProvider customAuthenticationProvider;
-
-	@Autowired
-	private AuthenticationSuccessHandler successHandler;
-
-	@Autowired
-	private AuthenticationFailHandler failHandler;
-
-	@Autowired
-	private LogoutHandler logoutHandler;
+	private JwtAuthorizationTokenFilter authenticationTokenFilter;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
+		.cors().and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
 		.authorizeRequests()
 		.antMatchers("/login").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin().loginPage("/login").loginProcessingUrl("/login")
-		.successHandler(successHandler).failureHandler(failHandler).permitAll()
-		.and()
-		.logout().logoutUrl("/logout")
-		.logoutSuccessHandler(logoutHandler).permitAll();
+		.antMatchers("/masterControl/getInviteRegisterSetting").permitAll()
+		.antMatchers("/userAccount/register").permitAll()
+		.antMatchers("/userAccount/getUserAccountInfo").permitAll()
+		.antMatchers("/game/findAllOpenGame").permitAll()
+		.antMatchers("/game/findAllGameCategory").permitAll()
+		.antMatchers("/game/findAllGameSituation").permitAll()
+		.antMatchers("/game/findGameSituationByGameCategoryId").permitAll()
+		.antMatchers("/recharge/muspayCallback").permitAll()
+		.antMatchers("/betting/findTop50WinningRank").permitAll()
+		.antMatchers("/systemNotice/findTop5PublishedSystemNotice").permitAll()
+		.antMatchers("/lotteryInformation/**").permitAll()
+		.antMatchers("/issue/findLotteryHistory").permitAll()
+		.antMatchers("/game/findGameByGameCode").permitAll()
+		.anyRequest().authenticated();
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**", "/images/**", "/js/**", "/plugins/**");
+		web.ignoring().antMatchers("/css/**", "/images/**", "/js/**", "/plugins/**", "/**.ico");
 	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-		builder.authenticationProvider(customAuthenticationProvider);
-	}
-
+	
 }
